8f44b9892672bdd9423cccd440be5f7bd86d15e7
Cody Tedrick <codytedrick@gmail.com>
2020-07-26 16:36:42 -0700

    initial commit

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace DefaultNamespace {
+	[CustomEditor(typeof(FieldOfView))]
+	public class FieldOfViewEditor : Editor {
+		private void OnSceneGUI() {
+			var fov = (FieldOfView) target;
+			Handles.color = Color.white;
+			var position = fov.transform.position;
+
+			Handles.DrawWireArc(position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+
+			var viewAngleA = fov.DirFromAngle(-fov.viewAngle / 2, false);
+			var viewAngleB = fov.DirFromAngle(fov.viewAngle / 2, false);
+
+			Handles.DrawLine(position, position + viewAngleA * fov.viewRadius);
+			Handles.DrawLine(position, position + viewAngleB * fov.viewRadius);
+
+			Handles.color = Color.red;
+			foreach (var target in fov.visibleTargets) {
+				Handles.DrawLine(fov.transform.position, target.position);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	[SerializeField] private float moveSpeed;
+
+	private Rigidbody rb;
+	private Camera viewCamera;
+	private Vector3 velocity;
+	
+	private void Start() {
+		rb = GetComponent<Rigidbody>();
+		viewCamera = Camera.main;
+	}
+
+	private void Update() {
+		var mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,viewCamera.transform.position.y));
+		transform.LookAt(mousePos + Vector3.up * transform.position.y);
+		velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * moveSpeed;
+	}
+
+	private void FixedUpdate() => rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+}
